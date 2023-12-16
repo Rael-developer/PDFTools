0eb68606c6620ef8589db55fe7f875417052f035
Israel Rodrigues <israelrodrigues2694@outlook.pt>
2023-12-15 23:05:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import menu_extrair
 import menu_marca_dagua
 import menu_imagens
 
-
-
 st.set_page_config(
     page_title='PDFTools',
     page_icon=':page_facing_up:',
@@ -20,16 +18,14 @@ with col2:
     st.title('PDFTools')
     st.markdown("""
     ### Escolha a opção desejada abaixo:      
-""")
-    
+    """)
+
 entradas_menu = {
-    'Extrair página':'file-earmark-pdf-fill',
-    'Combinar PDFs':'plus-square-fill',
-    "Adicionar marca d'água":'droplet-fill',
-    'Imagens para PDF':'file-earmark-richtext-fill',
+    'Extrair página': 'file-earmark-pdf-fill',
+    'Combinar PDFs': 'plus-square-fill',
+    "Adicionar marca d'água": 'droplet-fill',
+    'Imagens para PDF': 'file-earmark-richtext-fill',
 }
-
-
 
 escolha = option_menu(
     menu_title=None,
@@ -41,19 +37,12 @@ escolha = option_menu(
 
 _, col2, _ = st.columns(3)
 with col2:
-    match escolha:
-        case 'Extrair página':
-            menu_extrair.exibir_menu_extrair(coluna=col2)
-        case 'Combinar PDFs':
-            menu_combinar.exibir_menu_combinar(coluna=col2)
-        case "Adicionar marca d'água":
-            menu_marca_dagua.exibir_menu_marca_dagua(coluna=col2)
-        case 'Imagens para PDF':
-         st.write('Clicou imagens')
-         menu_imagens.exibir_menu_imagens(coluna=col2)  
-        
-        
-
-        
-    
-  
+    if escolha == 'Extrair página':
+        menu_extrair.exibir_menu_extrair(coluna=col2)
+    elif escolha == 'Combinar PDFs':
+        menu_combinar.exibir_menu_combinar(coluna=col2)
+    elif escolha == "Adicionar marca d'água":
+        menu_marca_dagua.exibir_menu_marca_dagua(coluna=col2)
+    elif escolha == 'Imagens para PDF':
+        st.write('Clicou imagens')
+        menu_imagens.exibir_menu_imagens(coluna=col2)
